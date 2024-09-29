bc492330ca39137e7c4d9a994c427bb8086523a2
ronald <ronald34n@gmail.com>
2024-09-29 10:50:04 -0400

    organizando en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { CounterComponent } from './counter/counter.component';
+import { EventComponent } from './event/event.component';
+import { FeatureComponent } from './feature/feature.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    PricingComponent,
+    ContactComponent,
+    AboutComponent,
+    CounterComponent,
+    EventComponent,
+    FeatureComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule
