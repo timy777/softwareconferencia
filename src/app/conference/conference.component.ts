a34d184ad856800466a744accba931989783a1f0
ronald <ronald34n@gmail.com>
2024-09-29 13:17:17 -0400

    modificacion componente conference

--- src/app/conference/conference.component.ts
+++ src/app/conference/conference.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   selector: 'app-conference',
   templateUrl: './conference.component.html',
   styleUrls: ['./conference.component.css']
 })
-export class ConferenceComponent {
+export class ConferenceComponent implements OnInit{
 
+
+  constructor(private socketService: SocketService) { 
+  }
+
+  ngOnInit(): void {
+    this.socketService.listen('mensaje').subscribe((data) => {
+      console.log('Mensaje recibido:', data);
+    });
+  }
+
+  enviarMensaje(): void {
+    // Emitir eventos
+    this.socketService.emit('mensaje', 'Hola desde Angular');
+  }
+  
 }
